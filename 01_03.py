f09605d23405312d51b51a736cb17609f7804edb
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-02 02:26:05 +0530

    add 01_03.py

--- 01_03.py
+++ 01_03.py
@@ -1,2 +1,9 @@
 # Distribute Candles
 
+class Solution:
+	def distributeCandles(self, candyType: List[int]) -> int:
+		types = len(set(candyType))
+		max_candies = len(candyType) // 2
+		
+		return min(types, max_candies)
+		
