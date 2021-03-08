f876bc6a6c9e8393afa865b37152c17da36fc5e6
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-08 14:04:27 +0530

    add comments 08_03.py

--- 08_03.py
+++ 08_03.py
@@ -1,9 +1,12 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
+        # if string is empty, return 0
         if s == '':
             return 0
         
+        # if string is a palindrome, one removal
         if s == s[::-1]:
             return 1
         
+        # else, one removal on a non palindrome non empty string (with only 2 characters) will return a palindromic string.
         return 2
