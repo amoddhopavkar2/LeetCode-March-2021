65306c4caee2081af577284eb95007252cb45d28
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-11 16:14:01 +0530

    add 11_03.py

---
+++ test.py
@@ -0,0 +1,20 @@
+class Table:
+    def __init__(self):
+        super().__init__()
+        tableNo = 0
+        waiterName = ''
+        status = ''
+
+
+def findWaiterWiseTotalNoOfTables(Table):
+    return 
+
+def findWaiterByTableNo(Table):
+    return 
+
+if __name__ == "__main__":
+	tables = int(input())
+    tableList = []
+    
+    for _ in range(tables):
+        self.Table.tableNo = int(input)
