9bac4123cc4df6050611f0841cc6ff973f9900e2
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-12 23:28:06 +0530

    add 12_03.py

--- 12_03.py
+++ 12_03.py
@@ -1,3 +1,6 @@
+# Friday, 12th March 2021
+# Check If a String Contains All Binary Codes of Size K
+
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         return len(set([s[i : i + k] for i in range(len(s) - k + 1)])) == pow(2, k)
