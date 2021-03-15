86e46a6ca224034fd507cbd393656d931f15a1fd
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-15 13:12:47 +0530

    add 13_03.py

---
+++ 14_03.py
@@ -0,0 +1,21 @@
+# Sunday, 14th March 2021
+# Swapping Nodes in a Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        A, B = head, head
+        for i in range(1, k):
+            A = A.next
+        nodeK, A = A, A.next
+        
+        while A:
+            A, B = A.next, B.next
+        nodeK.val, B.val = B.val, nodeK.val
+        
+        return head
