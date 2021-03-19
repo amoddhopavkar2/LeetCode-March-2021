dd5a217b29e3075bdcf19cc3f3d3ea737d5201c9
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-19 22:03:13 +0530

    stack based implementation

--- 19_03.py
+++ 19_03.py
@@ -1,2 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visitedRooms = set()
+        stack = [0]
+
+        while stack:
+            room = stack.pop()
+            visitedRooms.add(room)
+
+            for key in rooms[room]:
+                if key not in visitedRooms:
+                    stack.append(key)
+        
+        return len(visitedRooms) == len(rooms)
