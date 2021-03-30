c8d4a8c2dfe5f50834069e4680e873e0acfe649a
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-03-30 17:27:09 +0530

    add 30_03.py

---
+++ 30_03.py
@@ -0,0 +1,13 @@
+# Tuesday, 30th March 2021
+# Russian Doll Envelopes
+
+class Solution:
+    def maxEnvelopes(self, E: List[List[int]]) -> int:
+        E.sort(key=lambda x: (x[0], -x[1]))
+        dp = []
+        for _,height in E:
+            left = bisect_left(dp, height)
+            if left == len(dp): dp.append(height)
+            else: dp[left] = height
+        return len(dp)
+
